ff321a6c29e215e435103f2d186a9f69d7885385
Chris (Won Seok) Yoo <woyoo@umich.edu>
2017-03-22 14:59:55 +0900

    configuration

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+(function() {
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+            .html5Mode({
+            enabled: true,
+            requireBase: false
+            });
+    }
+    
+    angular
+        .module('blocChat', ['ui.router', 'firebase'])
+        .config(config);
+})();
